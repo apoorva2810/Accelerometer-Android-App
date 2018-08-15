db8aa1fc44da1e8038b3afa62c206b0820d9f48f
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-08-16 02:22:42 +0530

    Added accelerometer sensor in MainActivity

--- app/src/main/java/com/apoorvasingh2810/accelerometerapp/MainActivity.java
+++ app/src/main/java/com/apoorvasingh2810/accelerometerapp/MainActivity.java
@@ -1,13 +1,51 @@
 package com.apoorvasingh2810.accelerometerapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+/** SensorManager-Creates a instance of a Sensor Service */
+/** Sensor - is a class. It creates a specific type of sensor */
+/** SensorEvents - it provides a information about SensorEvents*/
+/** SensorEventListener - If sensorEvent occurs it will go to the SensorEventListener class */
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private static final String TAG = "MainActivity";
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"onCreate: Initializing Sensor Services");
+
+        sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener((SensorEventListener) MainActivity.this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG,"onCreate: Registered accelerometer listener");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        Log.d(TAG,"onSensorChanged: X: "+sensorEvent.values[0]+" Y: "+sensorEvent.values[1]+" Z: "+sensorEvent.values[2]);
     }
 }
